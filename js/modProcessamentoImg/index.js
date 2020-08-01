6c47f4f6e18afc529c5f588cbc7e01b08fffa8e6
hirvin-faria <hirvin.nogueira.faria@gmail.com>
2020-08-01 11:55:50 -0300

    :sparkles: Adicionando tratamento de contraste

--- js/modProcessamentoImg/index.js
+++ js/modProcessamentoImg/index.js
@@ -1,23 +1,39 @@
 let rangeBrightness = document.querySelector('#brightness');
-let labelBrightness = document.querySelector('#brightnessVelue');
+let labelBrightness = document.querySelector('#brightnessValue');
+
+let rangeContrast = document.querySelector('#contrast');
+let labelContrast = document.querySelector('#contrastValue');
+
 let bright = 0;
+let contrast = 0;
+let saturation = 0;
 
 rangeBrightness.value = bright;
-labelBrightness.innerHTML = bright;
+rangeContrast.value = contrast;
+
+labelBrightness.innerHTML = `Brightness: ${bright}`;
+labelContrast.innerHTML = `Contrast: ${contrast}`;
 
 Caman("#preprocss", function () {
     this.brightness(bright);
+    this.contrast(contrast);
     this.render();
 });
 
 rangeBrightness.addEventListener('input', function () {
-    //console.log(rangeBrightness.value)
     bright = rangeBrightness.value;
-    labelBrightness.innerHTML = bright
-
-    console.log(bright);
+    labelBrightness.innerHTML = `Brightness: ${bright}`
 
     Caman("#preprocss", function () {
         this.brightness(bright).render();
+    });
+}, false);
+
+rangeContrast.addEventListener('input', function () {
+    contrast = rangeContrast.value;
+    labelContrast.innerHTML = `Contrast: ${contrast}`
+
+    Caman("#preprocss", function () {
+        this.contrast(contrast).render();
     });
 }, false);
